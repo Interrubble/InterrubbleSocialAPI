a2d85d4f43f095ec9e5cee6b10b4b9e848512906
Interrubble <delagarzachris@icloud.com>
2022-05-25 13:06:34 -0700

    added thought model

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const {Schema, model} = require('mongoose');
+
+// Creathing thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => formatDate(date)
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [Reaction],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// Creating virtual property to get number of reactions
+thoughtSchema
+.virtual('reactionCount')
+.get(function(){
+    return this.reactions.length;
+});
+
+// Initializing thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;

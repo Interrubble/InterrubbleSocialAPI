80e373625c5fc55476b58d74b4e9cfa220eab5ed
Interrubble <delagarzachris@icloud.com>
2022-05-25 12:27:36 -0700

    added models folder and user model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const {Schema, model} = require('mongoose');
+
+// Creating user model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Creating virtual property to retrieve number of user's friends
+userSchema
+.virtual('friendCount')
+.get(function(){
+    return this.friends.length;
+});
+
+// Initializing user model
+const User = model('user', userSchema);
+
+module.exports = User;
